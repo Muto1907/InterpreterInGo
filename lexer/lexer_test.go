befd43b2a1f184d70f907bc471d2c3f8aa9c8a10
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-03 20:55:40 +0200

    Renew Testcase multichar keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -14,7 +14,17 @@ func TestNextToken(t *testing.T) {
 	};
 	let result = add(five, ten);
 	!-/*5;
-	5 < 10 > 5;`
+	5 < 10 > 5;
+	
+	if (5 < 10) {
+			return true;
+} else {
+		return false;
+}
+
+10 == 10;
+10 != 9;
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -68,6 +78,32 @@ func TestNextToken(t *testing.T) {
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.PARENL, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.PARENR, ")"},
+		{token.BRACEL, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.BRACER, "}"},
+		{token.ELSE, "else"},
+		{token.BRACEL, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.BRACER, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
 	}
 
 	l := New(input)
