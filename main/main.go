a1ceb0ffd5b17ddd7b0bcfade334d7961b3befd2
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-07 09:57:35 +0200

    Create REPL and Main Function

---
+++ main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/Muto1907/interpreterInGo/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! Welcome to the Monkey Programmin language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
