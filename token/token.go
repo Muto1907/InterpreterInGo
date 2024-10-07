0a1cb7157a0782160d60b1d87d0ae19c860431ae
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-07 09:08:27 +0200

    Extend token with keywords

--- token/token.go
+++ token/token.go
@@ -39,11 +39,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"func": FUNCTION,
-	"let":  LET,
+	"func":   FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func FindKeywordOrIdent(keyword string) TokenType {
