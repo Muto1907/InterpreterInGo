3a1df644c18279c0f43e2df092ec9da0c09e0ea5
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-18 18:41:59 +0200

    Begin Implementing Parsing for Let-Statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/Muto1907/interpreterInGo/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -23,4 +25,25 @@ func (p *Program) TokenLiteral() string {
 		return p.Statements[0].TokenLiteral()
 	}
 	return ""
+}
+
+type LetStatement struct {
+	Token      token.Token
+	Expression Expression
+	Name       *Identifier
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (id *Identifier) expressionNode() {}
+func (id *Identifier) TokenLiteral() string {
+	return id.Token.Literal
 }
