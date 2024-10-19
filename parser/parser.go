a9571756a441f97b34bd7bb47195574e2c92a673
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-19 13:29:28 +0200

    Create Parser struct with methods

---
+++ parser/parser.go
@@ -0,0 +1,28 @@
+package parser
+
+import (
+	"github.com/Muto1907/interpreterInGo/ast"
+	"github.com/Muto1907/interpreterInGo/lexer"
+	"github.com/Muto1907/interpreterInGo/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(lex *lexer.Lexer) Parser {
+	parser := &Parser{l: lex}
+	parser.nextToken()
+	parser.nextToken()
+}
+
+func (parser *Parser) nextToken() {
+	parser.currToken = parser.peekToken
+	parser.peekToken = parser.l.NextToken()
+}
+
+func (parser *Parser) ParseProgram() *ast.Program {
+	return nil
+}
