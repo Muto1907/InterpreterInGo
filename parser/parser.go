94b00ac6b9ee70884773895fe9729ca892b832e6
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-19 15:37:53 +0200

    Extend LetStetement Parsing with Parsing of Let and Identifier Token

--- parser/parser.go
+++ parser/parser.go
@@ -25,5 +25,55 @@ func (parser *Parser) nextToken() {
 }
 
 func (parser *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for parser.currToken.Type != token.EOF {
+		stmt := parser.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		parser.nextToken()
+	}
+	return program
+}
+
+func (parser *Parser) parseStatement() ast.Statement {
+	switch parser.currToken.Type {
+	case token.LET:
+		return parser.parseLetStatement()
+
+	default:
+		return nil
+	}
+}
+
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: parser.currToken}
+	if !parser.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: parser.currToken, Value: parser.currToken.Literal}
+	if !parser.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	for parser.currToken.Type != token.SEMICOLON {
+		parser.nextToken()
+	}
+	return stmt
+}
+
+func (parser *Parser) currentTokenIs(t token.TokenType) bool {
+	return parser.currToken.Type == t
+}
+
+func (parser *Parser) peekTokenIs(t token.TokenType) bool {
+	return parser.peekToken.Type == t
+}
+
+func (parser *Parser) expectPeek(t token.TokenType) bool {
+	if parser.peekTokenIs(t) {
+		parser.nextToken()
+		return true
+	}
+	return false
 }
