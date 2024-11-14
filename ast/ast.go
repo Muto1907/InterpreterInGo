682dd1ae593d23570872911eb43bcedf30c7f39e
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-11-14 17:30:40 +0100

    Implement AST Node for Call Expressions

--- ast/ast.go
+++ ast/ast.go
@@ -236,3 +236,25 @@ func (fn *FuncLiteral) String() string {
 	output.WriteString(fn.Body.String())
 	return output.String()
 }
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode() {}
+func (ce *CallExpression) TokenLiteral() string {
+	return ce.Token.Literal
+}
+func (ce *CallExpression) String() string {
+	var output bytes.Buffer
+	var args []string
+	for _, arg := range ce.Arguments {
+		args = append(args, arg.String())
+	}
+	output.WriteString(ce.Function.String() + "( ")
+	output.WriteString(strings.Join(args, ", "))
+	output.WriteString(")")
+	return output.String()
+}
