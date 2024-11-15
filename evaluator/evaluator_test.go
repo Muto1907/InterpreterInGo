bc593632c756c8e92d013fcb5cb9a904691e4a47
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-11-16 00:57:56 +0100

    Implement Testcase for upcoming Boolean Object evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -27,12 +27,43 @@ func TestEvalIntExpr(t *testing.T) {
 	}
 }
 
+func TestEvalBoolExpr(t *testing.T) {
+	tests := []struct {
+		input       string
+		expectedVal bool
+	}{
+		{
+			"true", true,
+		},
+		{
+			"false", false,
+		},
+	}
+
+	for _, tcase := range tests {
+		val := testEval(tcase.input)
+		testBooleanObject(t, val, tcase.expectedVal)
+	}
+}
+
 func testEval(input string) object.Object {
 	lex := lexer.New(input)
 	parser := parser.New(lex)
 	program := parser.ParseProgram()
 
 	return Eval(program)
+}
+
+func testBooleanObject(t *testing.T, booleanObject object.Object, expectedbool bool) bool {
+	res, ok := booleanObject.(*object.Boolean)
+	if !ok {
+		t.Fatalf("Object is not booleanObject. got=%T", booleanObject)
+	}
+	if res.Value != expectedbool {
+		t.Errorf("Unexpected Value of booleanObject. Expected=%t, got=%t", expectedbool, res.Value)
+		return false
+	}
+	return true
 }
 
 func testIntegerObject(t *testing.T, integerObject object.Object, expectedInt int64) bool {
