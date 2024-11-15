a77c1ebc44e81d5fb977bd4f99cdc1392b67f86f
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-11-15 23:34:21 +0100

    Implement Eval for IntegerObjects

---
+++ evaluator/evaluator.go
@@ -0,0 +1,27 @@
+package evaluator
+
+import (
+	"github.com/Muto1907/interpreterInGo/ast"
+	"github.com/Muto1907/interpreterInGo/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var obj object.Object
+
+	for _, stmt := range statements {
+		obj = Eval(stmt)
+	}
+	return obj
+}
