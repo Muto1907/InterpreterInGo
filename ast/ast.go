7ca1d06d84a8b8311cdb3fdbf000538a5369ddda
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-11-30 21:22:08 +0100

    Create AST Node for IndexExpressions

--- ast/ast.go
+++ ast/ast.go
@@ -293,3 +293,23 @@ func (al *ArrayLiteral) String() string {
 	output.WriteString("]")
 	return output.String()
 }
+
+type IndexExpression struct {
+	Token token.Token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode() {}
+func (ie *IndexExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+func (ie *IndexExpression) String() string {
+	var output bytes.Buffer
+	output.WriteString("(")
+	output.WriteString(ie.Left.String())
+	output.WriteString("[")
+	output.WriteString(ie.Index.String())
+	output.WriteString("])")
+	return output.String()
+}
