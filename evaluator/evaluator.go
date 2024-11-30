9389fb1718dd9bfeeadf4bd350761ee3b619fe7f
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-11-30 22:52:01 +0100

    Implement Evaluating of Arrays

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -71,6 +71,12 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return args[0]
 		}
 		return callFunction(function, args)
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
 	}
 
 	return nil
